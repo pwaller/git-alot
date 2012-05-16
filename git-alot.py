a1404073321832e5d1220029b914004a34aa94d5
Peter Waller <peter.waller@gmail.com>
2012-05-16 18:35:06 +0100

    Initial commit

---
+++ git-alot.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+
+from os.path import expandvars
+from commands import getstatusoutput
+
+import git
+
+# TODO
+
+# * Stashes
+# * Repository 'cleanliness'
+# * Find roots of repositories, so that repository copies can be identified?
+# * Remote syncing status?
+
+def find_git_repositories():
+    status, output = getstatusoutput(expandvars("find ${HOME} -iname .git"))
+    assert not status
+    return output.split("\n")
+
+def indent(t, i=4):
+    i = " "*i
+    return i + ("\n"+i).join(t.split("\n"))
+    
+class AlotRepo(object):
+    def __init__(self, repo):
+        self.repo = repo
+    
+    def __lt__(self, rhs):
+        # TODO some sort of cleanliness metric?
+        return self.repo.working_dir < rhs.repo.working_dir
+    
+    @property
+    def has_dirt(self):
+        # TODO check for stash
+        return self.repo.is_dirty(untracked_files=True)
+    
+    def __str__(self):
+        result = []; A = result.append
+        repo = self.repo
+        
+        A(repo.working_dir)
+        
+        if repo.is_dirty(index=False):
+            A("  == Dirty Working Tree ==")
+            A(indent(repo.git.diff(stat=True), i=3))
+            
+        if repo.is_dirty(working_tree=False):
+            A("  == Dirty Index (stuff to commit!) ==")
+            A(indent(repo.git.diff(stat=True, cached=True), i=3))
+            
+        untracked = repo.untracked_files
+        if untracked:
+            A("  == {0} Untracked file(s) ==".format(len(untracked)))
+            MAX = 10
+            A(indent("\n".join(untracked[:MAX])))
+            if len(untracked) > MAX:
+                A(indent("... and {0} more".format(len(untracked)-MAX)))
+        
+        return "\n".join(result)
+    
+def main():
+    print "Searching for repositories..",
+    repos = find_git_repositories()
+    print "Found {0}. That's alot of git.".format(len(repos))
+    
+    repos = map(git.Repo, repos)
+    repos = [r for r in repos if not r.bare]
+    
+    repos = map(AlotRepo, repos)
+    dirty_repos = [r for r in repos if r.has_dirt]
+    dirty_repos.sort()
+    
+    for repo in dirty_repos:
+        print repo
+        print
+    
+    nclean = len(repos) - len(dirty_repos)
+    if nclean:
+        print ".. you have", nclean, "clean repositories. Nice!"
+    else:
+        print "You have no clean repositories :-("
+
+if __name__ == "__main__":
+    main()
